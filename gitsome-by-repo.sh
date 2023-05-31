8580529143c11e2a3ab1e99cd51050a4364e59ce
savant <savant@savant42.com>
2023-05-31 00:20:37 -0700

    Finally working pretty well

--- gitsome-by-repo.sh
+++ gitsome-by-repo.sh
@@ -2,7 +2,9 @@
 # GraphQL Query to pull repository info from GitHub.com's Public API
 # Reqs: curl, jq, Github Token in GITHUB_TOKEN env var
 #
-# example: ./$0 owner repository
+# Usage: ./$0 owner repository
+# Example: ./gitsome-by-repo.sh facebook graphql
+# to enumerate information for https://github.com/facebook/graphql
 # todo: allow-list for safe characters only
 
 # Org or user that owns the repo, default to thesavant42/gitsome
@@ -59,12 +61,14 @@ data=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -s -d @- https://api.gith
       pullRequests(last: 100) {
         totalCount
         nodes {
+          number
           author{
             login
             url
             resourcePath
           }
           bodyText
+          permalink
         }
       }
       assignableUsers(last: 100) {
@@ -223,22 +227,62 @@ echo -e "
 Owner: $OWNER
 "
 
-jq -r '.data.repository |"
-Name (with Owner): \(.nameWithOwner)
-ID: \(.id)
-Description: \(.description)
-URL: \(.url)
-Homepage: \(.homepageUrl)
-Mirror URL: \(.mirrorUrl)
-Projects URL: \(.projectsUrl)
-Projects Count: \(.projects.totalCount)
-"' <<<$data
+jq -r '.data.repository |
+"Name (with Owner): \(.nameWithOwner)",
+"ID: \(.id)",
+"Description: \(.description)",
+"URL: \(.url)",
+"Homepage: \(.homepageUrl)",
+"Mirror URL: \(.mirrorUrl)",
+"Projects URL: \(.projectsUrl)",
+"Projects Count: \(.projects.totalCount)",
+"Disk Usage: \(.diskUsage)",
+"Has Wiki: \(.hasWikiEnabled)",
+"Is in Org: \(.isInOrganization)",
+"Is Empty: \(.isEmpty)",
+"Is Mirror: \(.isMirror)",
+"Is Fork: \(.isFork)",
+"Has Projects Enabled: \(.hasProjectsEnabled)",
+"Has Issues Enabled: \(.hasIssuesEnabled)",
+"Is User Config Repo: \(.isUserConfigurationRepository)",
+"\nTotal Stargazers: \(.stargazerCount)\n",
+"Stargazers: \(.stargazers.nodes[] | 
+  "Login: \(.login)", 
+  "Name: \(.name)", 
+  "Email: \(.email)", 
+  "URL: \(.url)\n")"
+' <<<$data
 
-echo -e "Assignable Users: $(jq -r .data.repository.assignableUsers.totalCount <<<$data)"
-
+#------
 # assignableUsers
+echo -e "\n[+] Assignable Users: $(jq -r .data.repository.assignableUsers.totalCount <<<$data)"
+
 jq -r '.data.repository.assignableUsers.nodes[]|
-"Assignable User Login: \(.login)", 
-"Assignable User Gists:", "\(.gists.edges[].node| "Name: \(.name)", "URL: \(.url)", "Description: \(.description)", "")",
+"\n[*] Assignable User Login: \(.login)", 
+"Assignable User Gists:",
+"", 
+"\(.gists.edges[].node| 
+  "\n[+] Gist Name: \(.name)", 
+  "URL: \(.url)", 
+  "Description: \(.description)", 
+  "Files: \(.files[]|
+  "Filename: \(.encodedName)", 
+  "Language: \(.language.name)", 
+  "Size: \(.size)")")",
+  "\nAssignable User Top Repositories - \(.login):", 
+    "\(.topRepositories.edges[].node| 
+      "\n[+] Top Repository Name: \(.name)", 
+      "URL: \(.url)", 
+      "Description: \(.description)")"' <<<$data
+
+
+# Pull Requests
+echo -e "\n[+] Pull Requests: $(jq -r .data.repository.pullRequests.totalCount <<<$data)"
+jq -r '.data.repository.pullRequests.nodes[]| 
+"PR Author Login: \(.author.login)", 
+"Author URL: \(.author.url)", 
+"PR Body Text: \n\(.bodyText)\n",
+"PR Permalink: \(.permalink)",
+"---", 
 ""
 ' <<<$data
